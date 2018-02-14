1af56b8aa21b2ce4035f1250884b2a73e2e6c4e7
JavaFXpert <james.l.weaver@gmail.com>
2018-02-13 19:00:59 -0500

    Incremental progress on implementing loop and active reset of qubits

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pyquil.quil import Program
+from pyquil.quilbase import RawInstr
 import pyquil.api as api
 from pyquil.gates import *
 import numpy as np
@@ -9,10 +10,8 @@ from gatedefs import *
 
 qvm = api.QVMConnection()
 p = Program()
-p.inst(X(0), Y(1), Z(0))
 
-angles = [180, 90, 127, 144, 148, 167, 171, 180, 126, 90, 131, 141, 172, 180, 180, 174, 123, 152, 180, 180, 136, 145, 90, 90, 138, 180, 123, 125]
-#angles = [0] * 28
+angles = [225,225,226,227,231,239,270,215,202,195,191,187,184,209,197,191,188,184,208,197,191,186,208,197,189,208,194,205]
 
 r = [0] * len(angles)
 for i in range(len(angles)):
@@ -20,12 +19,26 @@ for i in range(len(angles)):
 
 gate_matrix = compute_matrix(angles)
 
-p = Program().defgate("HARMONY_GATE", gate_matrix)
+p.defgate("ACCOMPANY_GATE", gate_matrix)
+p.inst(RawInstr("""
+DEFCIRCUIT CLEAR q scratch_bit:
+    MEASURE q scratch_bit
+    JUMP-UNLESS @end scratch_bit
+    X q
+    LABEL @end
+"""))
 
-p.inst(I(2), I(1), X(0)).inst(("HARMONY_GATE", 2, 1, 0)).measure(0, 0).measure(1, 1).measure(2, 2)
+p.inst(I(2), X(1), X(0))
+
+p.inst(RawInstr("""
+CLEAR 0 [4]
+"""))
+
+p.inst(("ACCOMPANY_GATE", 2, 1, 0))
+p.measure(0, 0).measure(1, 1).measure(2, 2)
 print(p)
 
-num_runs = 5
+num_runs = 1
 res = qvm.run(p, [2, 1, 0], num_runs)
 print(res)
 
