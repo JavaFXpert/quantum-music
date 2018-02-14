20e2ffe6494ce2a93a4d34451985be8130b60244
JavaFXpert <james.l.weaver@gmail.com>
2018-02-13 20:32:39 -0500

    Successfully using the measurement of qubits in a circuit as the starting qubits for another invocation of that circuit.

--- main.py
+++ main.py
@@ -20,26 +20,62 @@ for i in range(len(angles)):
 gate_matrix = compute_matrix(angles)
 
 p.defgate("ACCOMPANY_GATE", gate_matrix)
-p.inst(RawInstr("""
-DEFCIRCUIT CLEAR q scratch_bit:
-    MEASURE q scratch_bit
-    JUMP-UNLESS @end scratch_bit
-    X q
-    LABEL @end
-"""))
-
 p.inst(I(2), X(1), X(0))
 
 p.inst(RawInstr("""
-CLEAR 0 [4]
+DEFCIRCUIT ACTIVE-RESET q scratch_bit:
+    MEASURE q scratch_bit
+    JUMP-UNLESS @END-ACTIVE-SET scratch_bit
+    X q
+    LABEL @END-ACTIVE-SET
+
+DEFCIRCUIT ACTIVE-SET q scratch_bit:
+    MEASURE q scratch_bit
+    JUMP-WHEN @END-ACTIVE-RESET scratch_bit
+    X q
+    LABEL @END-ACTIVE-RESET
+
+ACCOMPANY_GATE 2 1 0
+
+MEASURE 2 [2]
+MEASURE 1 [1]
+MEASURE 0 [0]
+
+# Replicate the measurement results into qubits
+JUMP-UNLESS @RESET-Q2 [2]
+ACTIVE-SET 2 [63]
+JUMP @END-Q2
+LABEL @RESET-Q2
+ACTIVE-RESET 2 [63]
+LABEL @END-Q2 
+
+JUMP-UNLESS @RESET-Q1 [1]
+ACTIVE-SET 1 [63]
+JUMP @END-Q1
+LABEL @RESET-Q1
+ACTIVE-RESET 1 [63]
+LABEL @END-Q1 
+
+JUMP-UNLESS @RESET-Q0 [0]
+ACTIVE-SET 0 [63]
+JUMP @END-Q0
+LABEL @RESET-Q0
+ACTIVE-RESET 0 [63]
+LABEL @END-Q0 
+
+ACCOMPANY_GATE 2 1 0
+
+MEASURE 2 [5]
+MEASURE 1 [4]
+MEASURE 0 [3]
 """))
 
-p.inst(("ACCOMPANY_GATE", 2, 1, 0))
-p.measure(0, 0).measure(1, 1).measure(2, 2)
 print(p)
 
+
+
 num_runs = 1
-res = qvm.run(p, [2, 1, 0], num_runs)
+res = qvm.run(p, [2, 1, 0, 5, 4, 3], num_runs)
 print(res)
 
 
